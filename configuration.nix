13e90c72504e27851eee50ba708096d7c4d2f287
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-10 11:27:50 +0500

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,99 @@
+{ config, pkgs, ... }:
+
+let 
+  hm = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      (import "${hm}/nixos")
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Oral";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us,ru";
+    variant = "";
+    options = "grp:alt_shift_toggle";
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.sulya = {
+    isNormalUser = true;
+    description = "sulya";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  home-manager.users.sulya = { pkgs, ... } : {
+    home.packages = with pkgs; [
+      firefox
+      neovim
+      git
+    ];
+
+
+
+    home.stateVersion = "25.05";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+
+  ];
+
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+
+
+  system.stateVersion = "25.05";
+}
