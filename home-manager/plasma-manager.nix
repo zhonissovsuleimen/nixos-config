6a62c206cc7276a037e08985cd37e90513c5931b
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-10 17:56:01 +0500

    panel widgets

--- home-manager/plasma-manager.nix
+++ home-manager/plasma-manager.nix
@@ -33,11 +33,20 @@ in
     };
 
 
-      panels = [
-        {
-          floating = false;
-	  opacity = "opaque";
-	}
-      ];
+    panels = [
+      {
+        floating = false;
+	opacity = "opaque";
+	widgets = [
+	  "org.kde.plasma.kickoff"
+	  #"org.kde.plasma.pager"
+	  "org.kde.plasma.icontasks"
+	  "org.kde.plasma.marginsseparator"
+	  "org.kde.plasma.systemtray"
+	  "org.kde.plasma.digitalclock"
+	  #"org.kde.plasma.showdesktop"
+	];
+      }
+    ];
   };
 }
