73efa195b3aa6c16d4938fa49cff6b96d37f9cb5
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-11 15:23:28 +0500

    tabbing

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -9,6 +9,12 @@
       set number
       set relativenumber
       set clipboard+=unnamedplus
+
+      set expandtab
+      set shiftwidth=2
+      set tabstop=2
+      set softtabstop=2
+      inoremap <S-Tab> <C-d>
     '';
   };
 }
