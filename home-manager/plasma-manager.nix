cf45d1837d489929df9f23eca3c642978f76fbcd
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-11 16:12:26 +0500

    krunner plguins

--- home-manager/plasma-manager.nix
+++ home-manager/plasma-manager.nix
@@ -99,6 +99,26 @@ in
       spectacle."ImageSave"."imageCompressionQuality" = 100;
 
       krunnerrc."General"."activateWhenTypingOnDesktop" = false;
+      krunnerrc."Plugins"."browserhistoryEnabled" = false;
+      krunnerrc."Plugins"."browsertabsEnabled" = false;
+      krunnerrc."Plugins"."calculatorEnabled" = false;
+      krunnerrc."Plugins"."helprunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_appstreamEnabled" = false;
+      krunnerrc."Plugins"."krunner_bookmarksrunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_charrunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_dictionaryEnabled" = false;
+      krunnerrc."Plugins"."krunner_katesessionsEnabled" = false;
+      krunnerrc."Plugins"."krunner_killEnabled" = false;
+      krunnerrc."Plugins"."krunner_konsoleprofilesEnabled" = false;
+      krunnerrc."Plugins"."krunner_placesrunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_recentdocumentsEnabled" = false;
+      krunnerrc."Plugins"."krunner_sessionsEnabled" = false;
+      krunnerrc."Plugins"."krunner_spellcheckEnabled" = false;
+      krunnerrc."Plugins"."krunner_webshortcutsEnabled" = false;
+      krunnerrc."Plugins"."org.kde.activities2Enabled" = false;
+      krunnerrc."Plugins"."org.kde.datetimeEnabled" = false;
+      krunnerrc."Plugins"."unitconverterEnabled" = false;
+      krunnerrc."Plugins"."windowsEnabled" = false;
     };
 
   };
