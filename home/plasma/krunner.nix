5724f575936ed5116e77fe6172c3797196216b2a
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-18 04:12:39 +0500

    krunner shortcuts

--- home/plasma/krunner.nix
+++ home/plasma/krunner.nix
@@ -1,27 +1,34 @@
 { ... }:
 
 {
-  programs.plasma.configFile = {
-    krunnerrc."General"."ActivateWhenTypingOnDesktop" = false;
-    krunnerrc."Plugins"."browserhistoryEnabled" = false;
-    krunnerrc."Plugins"."browsertabsEnabled" = false;
-    krunnerrc."Plugins"."calculatorEnabled" = false;
-    krunnerrc."Plugins"."helprunnerEnabled" = false;
-    krunnerrc."Plugins"."krunner_appstreamEnabled" = false;
-    krunnerrc."Plugins"."krunner_bookmarksrunnerEnabled" = false;
-    krunnerrc."Plugins"."krunner_charrunnerEnabled" = false;
-    krunnerrc."Plugins"."krunner_dictionaryEnabled" = false;
-    krunnerrc."Plugins"."krunner_katesessionsEnabled" = false;
-    krunnerrc."Plugins"."krunner_killEnabled" = false;
-    krunnerrc."Plugins"."krunner_konsoleprofilesEnabled" = false;
-    krunnerrc."Plugins"."krunner_placesrunnerEnabled" = false;
-    krunnerrc."Plugins"."krunner_recentdocumentsEnabled" = false;
-    krunnerrc."Plugins"."krunner_sessionsEnabled" = false;
-    krunnerrc."Plugins"."krunner_spellcheckEnabled" = false;
-    krunnerrc."Plugins"."krunner_webshortcutsEnabled" = false;
-    krunnerrc."Plugins"."org.kde.activities2Enabled" = false;
-    krunnerrc."Plugins"."org.kde.datetimeEnabled" = false;
-    krunnerrc."Plugins"."unitconverterEnabled" = false;
-    krunnerrc."Plugins"."windowsEnabled" = false;
+  programs.plasma = {
+    krunner.shortcuts = {
+      launch = "none";
+      runCommandOnClipboard = "none";
+    };
+
+    configFile = {
+      krunnerrc."General"."ActivateWhenTypingOnDesktop" = false;
+      krunnerrc."Plugins"."browserhistoryEnabled" = false;
+      krunnerrc."Plugins"."browsertabsEnabled" = false;
+      krunnerrc."Plugins"."calculatorEnabled" = false;
+      krunnerrc."Plugins"."helprunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_appstreamEnabled" = false;
+      krunnerrc."Plugins"."krunner_bookmarksrunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_charrunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_dictionaryEnabled" = false;
+      krunnerrc."Plugins"."krunner_katesessionsEnabled" = false;
+      krunnerrc."Plugins"."krunner_killEnabled" = false;
+      krunnerrc."Plugins"."krunner_konsoleprofilesEnabled" = false;
+      krunnerrc."Plugins"."krunner_placesrunnerEnabled" = false;
+      krunnerrc."Plugins"."krunner_recentdocumentsEnabled" = false;
+      krunnerrc."Plugins"."krunner_sessionsEnabled" = false;
+      krunnerrc."Plugins"."krunner_spellcheckEnabled" = false;
+      krunnerrc."Plugins"."krunner_webshortcutsEnabled" = false;
+      krunnerrc."Plugins"."org.kde.activities2Enabled" = false;
+      krunnerrc."Plugins"."org.kde.datetimeEnabled" = false;
+      krunnerrc."Plugins"."unitconverterEnabled" = false;
+      krunnerrc."Plugins"."windowsEnabled" = false;
+    };
   };
 }
