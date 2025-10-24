9f2fe1a9b86dea8285dbca03f9ee8a8c3b9d6e2b
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-24 12:35:23 +0500

    add missing semicolons

--- home/default.nix
+++ home/default.nix
@@ -13,9 +13,9 @@
     shellAliases = {
       nrs = "sudo nixos-rebuild switch --flake ~/.nixos-config/";
       nrt = "sudo nixos-rebuild test --flake ~/.nixos-config/";
-      gs = "git status"
-      gd = "git diff"
-      gds = "git diff --staged"
+      gs = "git status";
+      gd = "git diff";
+      gds = "git diff --staged";
     };
   };
 
