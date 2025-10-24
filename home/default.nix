9ace03af209d1cda5ca5abb0bde22fddd2f276eb
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-24 12:29:21 +0500

    bash; shell aliases

--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,17 @@
     telegram-desktop
   ];
 
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch --flake ~/.nixos-config/";
+      nrt = "sudo nixos-rebuild test --flake ~/.nixos-config/";
+      gs = "git status"
+      gd = "git diff"
+      gds = "git diff --staged"
+    };
+  };
+
   imports = [
     ./plasma
     ./vesktop
