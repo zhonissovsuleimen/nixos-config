d9d7d56594fd8ceefa23779527d74546daf885a5
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-24 12:22:14 +0500

    exclude default plasma6 packages

--- nixos/programs/default.nix
+++ nixos/programs/default.nix
@@ -6,6 +6,12 @@
     nvtopPackages.nvidia
   ];
 
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    kate
+    okular
+    elisa
+  ];
+
   programs.steam = {
     enable = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
