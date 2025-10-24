a78a4a59aef665c38f2e8f708a45d964c11c975c
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-24 12:36:04 +0500

    KScreen desktop scale factor

--- home/plasma/kdeglobals.nix
+++ home/plasma/kdeglobals.nix
@@ -5,6 +5,8 @@
     configFile = {
       kdeglobals."General"."TerminalApplication" = "alacritty";
       kdeglobals."General"."TerminalService" = "Alacritty.desktop";
+
+      kdeglobals."KScreen"."ScaleFactor" = 1.0;
     };
   };
 }
