a51e94702f4e9e8211334b2b872dd60e0ebea48a
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-01-15 16:00:53 +0500

    quick launch aliases

--- modules/apps/bash.nix
+++ modules/apps/bash.nix
@@ -2,11 +2,21 @@
   programs.bash = {
     enable = true;
     shellAliases = {
+      #nixos
       nrs = "sudo nixos-rebuild switch --flake ~/.nixos-config/";
       nrt = "sudo nixos-rebuild test --flake ~/.nixos-config/";
+
+      #git
       gs = "git status";
       gd = "git diff";
       gds = "git diff --staged";
+
+      #quick launch
+      ff = "firefox -foreground & exit";
+      ffp = "firefox -foreground -private-window & exit";
+      yt = "firefox -foreground -new-window youtube.com & exit";
+      gpt = "firefox -foreground -new-window chatgpt.com & exit";
+      tg = "Telegram & exit";
     };
   };
 }
