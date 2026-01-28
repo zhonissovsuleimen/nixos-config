d1ed47f1fe50da2b12d21e96c9017311ee7ac1e2
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-01-28 05:40:06 +0500

    network manager + wifi

--- modules/os/network.nix
+++ modules/os/network.nix
@@ -1,29 +1,25 @@
 {
-  #networking.hostName = "sulya-linux";
-  #networking.networkmanager.enable = true;
   networking = {
     hostName = "sulya-linux";
-    useDHCP = false;
 
-    networkmanager.enable = false;
-    wireless.enable = false;
-    resolvconf.enable = false;
+    networkmanager.enable = true;
+    wireless.enable = true;
   };
 
-  systemd = {
-    network = {
-      enable = true;
-      networks."10-enp8s0" = {
-        matchConfig.Name = "enp8s0";
-        address = [ "192.168.100.51/24" ];
-        routes = [ { Gateway = "192.168.100.1"; } ];
-        dns = [ "8.8.8.8" "8.8.4.4" ];
-        linkConfig.RequiredForOnline = "routable";
-      };
-
-      wait-online.enable = false;
-    };
-
-    services.systemd-networkd-persistent-storage.enable = false;
-  };
+  # systemd = {
+  #   network = {
+  #     enable = true;
+  #     networks."10-enp8s0" = {
+  #       matchConfig.Name = "enp8s0";
+  #       address = [ "192.168.100.51/24" ];
+  #       routes = [ { Gateway = "192.168.100.1"; } ];
+  #       dns = [ "8.8.8.8" "8.8.4.4" ];
+  #       linkConfig.RequiredForOnline = "routable";
+  #     };
+  #
+  #     wait-online.enable = false;
+  #   };
+  #
+  # #   services.systemd-networkd-persistent-storage.enable = false;
+  # };
 }
