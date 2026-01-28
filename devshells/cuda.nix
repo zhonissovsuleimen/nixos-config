624e447b27674198a2b9aef633c43e052a72f53b
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-01-28 06:29:42 +0500

    env vars & nvcc flags

--- devshells/cuda.nix
+++ devshells/cuda.nix
@@ -6,6 +6,12 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
+    alias nvcc="nvcc --cudart shared -arch=sm_89"
+
+    export PATH=${pkgs.cudaPackages.cudatoolkit}/bin:$PATH
+    export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib64:$LD_LIBRARY_PATH
+    export CUDA_HOME=${pkgs.cudaPackages.cudatoolkit}
+
     nvcc --version
   '';
 }
