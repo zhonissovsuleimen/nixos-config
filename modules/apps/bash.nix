48fbcd7947c4dcbcde84b3ccd0aab661626490ee
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-02-10 17:20:21 +0500

    chess quick launch

--- modules/apps/bash.nix
+++ modules/apps/bash.nix
@@ -12,10 +12,12 @@
       gds = "git diff --staged";
 
       #quick launch
+      chess = "firefox -foreground -new-window chess.com -new-tab lichess.org/paste & exit";
       ff = "firefox -foreground & exit";
       ffp = "firefox -foreground -private-window & exit";
-      yt = "firefox -foreground -new-window youtube.com & exit";
       gpt = "firefox -foreground -new-window chatgpt.com & exit";
+      yt = "firefox -foreground -new-window youtube.com & exit";
+
       tg = "Telegram & exit";
     };
   };
